6e3139e37a5a1da3ab59fc42e19feb7e7a3d5972
Luka Stosic <lukastosic39@gmail.com>
2023-04-13 15:22:01 +0200

    versioning

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('album', \App\Http\Controllers\AlbumController::class);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('album', \App\Http\Controllers\AlbumController::class);
+});
