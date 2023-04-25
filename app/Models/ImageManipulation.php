bdfd1e914c2b3070e2103ad9f1a1c9dde88fbfbd
Luka <hp@192.168.1.48>
2023-04-25 15:16:20 +0200

    Models UPDATED_AT can accept only integer or string, so instead of false I set it to null if i dont wanna use updated_at in database

--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -11,8 +11,9 @@ class ImageManipulation extends Model
     
     const TYPE_RESIZE = 'resize';
     
-    //not using in database ORM will ignore updated_at while inserting data
-    const UPDATED_AT = false;
+    //not using in database ORM will ignore updated_at while inserting data.
+    //can accept only integer or string, so instead of false i set it to null
+    const UPDATED_AT = null;
     
     protected $filable = ['name', 'path', 'type', 'data', 'output_path', 'user_id', 'album_id'];
 }
