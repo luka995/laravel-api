5c1edf56930123401c486f45873003d9c69023c6
Luka <>
2023-04-25 14:55:30 +0200

    image request rules and menage image controller for saving images from url or uploaded image from device

--- app/Http/Controllers/V1/ImageManipulationController.php
+++ app/Http/Controllers/V1/ImageManipulationController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ResizeImageRequest;
 use App\Models\ImageManipulation;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
 use App\Models\Album;
 
 class ImageManipulationController extends Controller
@@ -27,7 +29,49 @@ class ImageManipulationController extends Controller
      */
     public function resize(ResizeImageRequest $request)
     {
-        //
+        $all = $request->all();
+        
+        /** @var UploadedFile|string file or Url $image */
+        $image = $all['image'];
+        
+        //unseting image from all because $all will be used to save data to database
+        //image does not exist in database
+        unset($all['image']);
+        
+        $data = [
+            'type' => ImageManipulation::TYPE_RESIZE,
+            'data' => json_encode($all),
+            'user_id' => null
+        ];
+        
+        if (isset($all['album_id'])) {
+            //TODO after implementing authentication
+            
+            $data['album_id'] = $all['album_id'];
+        }
+        
+        $dir = 'images/'.\Illuminate\Support\Str::random().'/';
+        $absolutePath = public_path($dir);
+        
+        File::makeDirectory($absolutePath);
+        
+        if ($image instanceof UploadedFile) {
+            $data['name'] = $image->getClientOriginalName();
+            
+            $filename = pathinfo($data['name'], PATHINFO_FILENAME);
+            $extension = $image->getClientOriginalExtension();
+            $originalPath = $absolutePath.$data['name'];
+            
+            $image->move($absolutePath, $data['name']);
+        } else {
+            $data['name'] = pathinfo($image, PATHINFO_BASENAME);            
+            $filename = pathinfo($image, PATHINFO_FILENAME);
+            $extension = pathinfo($image, PATHINFO_EXTENSION);
+            $originalPath = $absolutePath.$data['name'];
+            
+            copy($image, $absolutePath.$data['name']);
+        }
+        $data['path'] = $dir.$data['name'];
     }
 
     /**
--- app/Models/ImageManipulation.php
+++ app/Models/ImageManipulation.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ImageManipulation extends Model
 {
     use HasFactory;
+    
+    const TYPE_RESIZE = 'resize';
+    
+    //not using in database ORM will ignore updated_at while inserting data
+    const UPDATED_AT = false;
+    
+    protected $filable = ['name', 'path', 'type', 'data', 'output_path', 'user_id', 'album_id'];
 }
